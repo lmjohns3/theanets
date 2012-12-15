6976862f0546e3638e618640a6ca0e0d8acc551d
Leif Johnson <leif@leifjohnson.net>
2012-12-14 21:15:33 -0600

    Add basic files for namespaced package installation.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+import setuptools
+
+setuptools.setup(
+    name='lmj.tnn',
+    version='0.1.0',
+    namespace_packages=['lmj'],
+    packages=setuptools.find_packages(),
+    author='Leif Johnson',
+    author_email='leif@leifjohnson.net',
+    description='A library of neural nets in theano',
+    long_description=open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')).read(),
+    license='MIT',
+    url='http://github.com/lmjohns3/theano-nets/',
+    keywords=('deep-belief-network '
+              'neural-network '
+              'theano '
+              'machine-learning'),
+    install_requires=['theano'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        ],
+    )

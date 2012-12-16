fcdf5a175049ef70eeea8df171a39beaf2b8370d
Leif Johnson <leif@leifjohnson.net>
2012-12-15 18:54:15 -0600

    Add basic network subclasses to the recurrent module.

--- lmj/tnn/recurrent.py
+++ lmj/tnn/recurrent.py
@@ -23,29 +23,29 @@
 import numpy
 import numpy.random as rng
 import theano
-import theano.tensor as T
+import theano.tensor as TT
 
-import .feedforward
+from . import feedforward as ff
 
 
-class Network(feedforward.Network):
+class Network(ff.Network):
     '''A fully connected recurrent network with inputs and outputs.'''
 
     def __init__(self, layers, nonlinearity=TT.nnet.sigmoid, gain=1.2, damping=0.01):
-        self.x = T.matrix('x')
+        self.x = TT.matrix('x')
         self.state = theano.shared(
-            numpy.zeros(size, dtype=feedforward.FLOAT), name='state')
+            numpy.zeros(size, dtype=ff.FLOAT), name='state')
 
         i, h, o = layers
         arr = rng.normal(size=(i, h)) / numpy.sqrt(i + h)
-        W_in = theano.shared(arr.astype(feedforward.FLOAT), name='W_in')
+        W_in = theano.shared(arr.astype(ff.FLOAT), name='W_in')
 
         arr = gain * rng.normal(size=(h, h)) / numpy.sqrt(h + h)
-        W_pool = theano.shared(arr.astype(feedforward.FLOAT), name='W_pool')
+        W_pool = theano.shared(arr.astype(ff.FLOAT), name='W_pool')
 
         arr = rng.normal(size=(h, o)) / numpy.sqrt(h + o)
-        W_out = theano.shared(arr.astype(feedforward.FLOAT), name='W_out')
-        b_out = theano.shared(numpy.zeros((o, ), feedforward.FLOAT), name='b_out')
+        W_out = theano.shared(arr.astype(ff.FLOAT), name='W_out')
+        b_out = theano.shared(numpy.zeros((o, ), ff.FLOAT), name='b_out')
 
         self.hiddens = [self.state]
         self.weights = [W_in, W_pool, W_out]
@@ -53,9 +53,9 @@ class Network(feedforward.Network):
 
         logging.info('%d total network parameters', h * (i + h + o) + o)
 
-        z = nonlinearity(T.dot(self.x, W_in) + T.dot(self.state, W_pool) + b_pool)
+        z = nonlinearity(TT.dot(self.x, W_in) + TT.dot(self.state, W_pool) + b_pool)
         self.next_state = damping * self.state + (1 - damping) * z
-        self.y = T.dot(self.next_state, W_out) + b_out
+        self.y = TT.dot(self.next_state, W_out) + b_out
 
         self.f = theano.function(*self.args, updates={self.state: self.next_state})
 
@@ -66,3 +66,64 @@ class Network(feedforward.Network):
     @property
     def args(self):
         return [self.x], [self.y]
+
+
+class Autoencoder(Network):
+    '''An autoencoder attempts to reproduce its input.'''
+
+    @property
+    def cost(self):
+        err = self.y - self.x
+        return TT.mean((err * err).sum(axis=1))
+
+
+class Regressor(Network):
+    '''A regressor attempts to produce a target output.'''
+
+    def __init__(self, *args, **kwargs):
+        super(Regressor, self).__init__(*args, **kwargs)
+        self.k = TT.matrix('k')
+
+    @property
+    def inputs(self):
+        return [self.x, self.k]
+
+    @property
+    def cost(self):
+        err = self.k - self.y
+        return TT.mean((err * err).sum(axis=1))
+
+
+class Classifier(Network):
+    '''A classifier attempts to match a 1-hot target output.'''
+
+    def __init__(self, *args, **kwargs):
+        super(Classifier, self).__init__(*args, **kwargs)
+        self.y = self.softmax(self.y)
+        self.k = TT.ivector('k')
+
+    @staticmethod
+    def softmax(x):
+        # TT.nnet.softmax doesn't work with the HF trainer.
+        z = TT.exp(x - x.max(axis=1)[:, None])
+        return z / z.sum(axis=1)[:, None]
+
+    @property
+    def inputs(self):
+        return [self.x, self.k]
+
+    @property
+    def prediction(self):
+        return TT.argmax(self.y, axis=1)
+
+    @property
+    def cost(self):
+        return -TT.mean(TT.log(self.y)[TT.arange(self.k.shape[0]), self.k])
+
+    @property
+    def incorrect(self):
+        return TT.mean(TT.neq(self.prediction, self.k))
+
+    @property
+    def monitors(self):
+        return [self.incorrect] + self.sparsities

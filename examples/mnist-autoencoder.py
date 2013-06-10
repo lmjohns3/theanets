b56a5d21cf1de01d97a1d72d221bd2b3ee528f49
Leif Johnson <leif@leifjohnson.net>
2013-06-10 12:53:42 -0500

    Update examples to generate plots of learned features.

--- examples/mnist-autoencoder.py
+++ examples/mnist-autoencoder.py
@@ -5,6 +5,8 @@ import gzip
 import logging
 import lmj.cli
 import lmj.nn
+import matplotlib.pyplot as plt
+import numpy as np
 import os
 import tempfile
 import urllib
@@ -22,12 +24,16 @@ if not os.path.isfile(DATASET):
 train, valid, _ = [x for x, _ in cPickle.load(gzip.open(DATASET))]
 
 e = lmj.nn.Experiment(lmj.nn.Autoencoder, layers=(784, 200, 784))
+e.run(train, valid)
 
-path = os.path.join(
-    tempfile.gettempdir(),
-    'mnist-autoencoder-%s.pkl.gz' % ','.join(str(n) for n in m.args.layers))
-
-if os.path.exists(path):
-    e.load(path)
-e.train(train, valid)
-e.save(path)
+# make a plot showing the learned basis functions as 28x28 images.
+img = np.zeros((231, 231), float)
+for i, d in enumerate(e.network.weights[0].get_value().T):
+    if i == 64:
+        break
+    a, b = divmod(i, 8)
+    img[a * 29:a * 29 + 28, b * 29:b * 29 + 28] = d.reshape((28, 28))
+ax = plt.subplot(111)
+ax.set_frame_on(False)
+ax.imshow(img, cmap='gray')
+plt.show()
--- examples/mnist-classifier.py
+++ examples/mnist-classifier.py
@@ -5,6 +5,8 @@ import gzip
 import logging
 import lmj.cli
 import lmj.nn
+import matplotlib.pyplot as plt
+import numpy as np
 import os
 import tempfile
 import urllib
@@ -23,12 +25,16 @@ train, valid, _ = [
     (x, y.astype('int32')) for x, y in cPickle.load(gzip.open(DATASET))]
 
 e = lmj.nn.Experiment(lmj.nn.Classifier, layers=(784, 200, 10))
-
-path = os.path.join(
-    tempfile.gettempdir(),
-    'mnist-classifier-%s.pkl.gz' % ','.join(str(n) for n in e.args.layers))
-
-if os.path.exists(path):
-    e.load(path)
 e.run(train, valid)
-e.save(path)
+
+# make a plot showing the learned basis functions as 28x28 images.
+img = np.zeros((231, 231), float)
+for i, d in enumerate(e.network.weights[0].get_value().T):
+    if i == 64:
+        break
+    a, b = divmod(i, 8)
+    img[a * 29:a * 29 + 28, b * 29:b * 29 + 28] = d.reshape((28, 28))
+ax = plt.subplot(111)
+ax.set_frame_on(False)
+ax.imshow(img, cmap='gray')
+plt.show()

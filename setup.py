922a411bf069ae68e1f1f9b865d677124fd4abd2
Leif Johnson <leif@leifjohnson.net>
2014-01-23 11:56:59 -0600

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='theanets',
-    version='0.1.1',
+    version='0.1.2',
     packages=setuptools.find_packages(),
     author='Leif Johnson',
     author_email='leif@leifjohnson.net',

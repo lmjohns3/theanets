57f1eb3325cff9840955fe019057e02e48e62a5c
Leif Johnson <leif@leifjohnson.net>
2014-02-28 22:24:45 -0600

    Update recurrent network to work with minibatches -- might not work fully yet, but the bulk of the computation is there. Also, add a class for predicting one-step future states.

--- theanets/recurrent.py
+++ theanets/recurrent.py
@@ -38,7 +38,7 @@ class Network(ff.Network):
 
     Parameters
     ----------
-    layers : (int, int, int)
+    layers : sequence of int
         A sequence of three integers specifying the number of units in the
         input, hidden, and output layers, respectively.
 
@@ -64,11 +64,6 @@ class Network(ff.Network):
     hidden_dropouts : float, optional
         Proportion of hidden unit activations to randomly set to 0.
 
-    pool_damping : float in [0, 1], optional
-        This parameter governs the proportion of past state information retained
-        by the recurrent pool neurons. A value of 0 causes the network to retain
-        zero past state, while a value of 1 accumulates all past state.
-
     pool_noise : float, optional
         Add gaussian noise to recurrent pool neurons with this variance.
 
@@ -80,16 +75,13 @@ class Network(ff.Network):
         Compute error metrics starting at this time step. (Defaults to 3.)
     '''
 
-    def __init__(self, layers, activation, rng=None, input_noise=0,
-                 hidden_noise=0, input_dropouts=0, hidden_dropouts=0, **kwargs):
+    def __init__(self, layers, activation, rng=None, **kwargs):
         self.rng = rng or RandomStreams()
-        pool_noise = kwargs.get('pool_noise', 0.)
-        pool_dropouts = kwargs.get('pool_dropouts', 0.)
-        pool_damping = kwargs.get('pool_damping', 0.)
         self.error_start = kwargs.get('pool_error_start', 3)
 
-        # in this module, x refers to a network's input, and y to its output.
-        self.x = TT.matrix('x')
+        # the first dimension indexes time, the second indexes the elements of
+        # each minibatch, and the third indexes the variables in a given frame.
+        self.x = TT.tensor3('x')
 
         parameter_count = 0
         layers = list(layers)
@@ -99,40 +91,43 @@ class Network(ff.Network):
         W_in = []
         b_in = []
         for i, (a, b) in enumerate(zip(layers, layers[1:] + [num_pool])):
-            W, b, params = self._weights_and_bias(a, b, i)
-            parameter_count += params
+            W, b, count = self._create_layer(a, b, i)
+            parameter_count += count
             W_in.append(W)
             b_in.append(b)
 
-        # discard bias for last input layer (we create it explicitly below, as
-        # it overlaps with the bias for the recurrent pool)
-        b_in.pop()
-        parameter_count -= num_pool
+        # we give a special name to the bias for the recurrent pool units.
+        b_pool = b_in.pop()
 
-        W_pool, b_pool, params = self._weights_and_bias(num_pool, num_pool, 'pool')
-        parameter_count += params
+        W_pool, _, count = self._create_layer(num_pool, num_pool, 'pool')
+        parameter_count += count - num_pool
 
-        W_out, b_out, params = self._weights_and_bias(num_pool, num_out, 'out')
-        parameter_count += params
+        W_out, b_out, count = self._create_layer(num_pool, num_out, 'out')
+        parameter_count += count
 
         logging.info('%d total network parameters', parameter_count)
 
         def recurrence(x_t, h_tm1):
-            z = self._add_noise(x_t, input_noise, input_dropouts)
-            encs = []
+            z = [self._add_noise(x_t,
+                                 kwargs.get('input_noise', 0.),
+                                 kwargs.get('input_dropouts', 0.))]
             for W, b in zip(W_in, b_in):
-                encs.append(self._add_noise(
-                    activation(TT.dot(z, W) + b), hidden_noise, hidden_dropouts))
-                z = encs[-1]
-            h = activation(TT.dot(z, W_in[-1]) + TT.dot(h_tm1, W_pool) + b_pool)
-            h_t = self._add_noise(
-                (1 - pool_damping) * h + pool_damping * h_tm1,
-                pool_noise, pool_dropouts)
-            return encs + [h_t, TT.dot(h_t, W_out) + b_out]
+                z.append(self._add_noise(
+                    activation(TT.dot(z[-1], W) + b),
+                    kwargs.get('hidden_noise', 0.),
+                    kwargs.get('hidden_dropouts', 0.)))
+            z.append(self._add_noise(activation(
+                TT.dot(z[-1], W_in[-1]) + TT.dot(h_tm1, W_pool) + b_pool),
+                kwargs.get('pool_noise', 0.),
+                kwargs.get('pool_dropouts', 0.)))
+            z.append(TT.dot(z[-1], W_out) + b_out)
+            return z[1:]
 
-        h_0 = TT.zeros((num_pool, ), dtype=ff.FLOAT)
+        batch_size = kwargs.get('batch_size', 64)
+        h_0 = TT.zeros((batch_size, num_pool), dtype=ff.FLOAT)
         outputs, self.updates = theano.scan(
-            fn=recurrence, sequences=self.x,
+            fn=recurrence,
+            sequences=self.x,
             outputs_info=[None for _ in b_in] + [h_0, None])
 
         self.y = outputs.pop()
@@ -154,12 +149,21 @@ class Autoencoder(Network):
         return TT.mean((err * err).sum(axis=1)[self.error_start:])
 
 
+class Predictor(Autoencoder):
+    '''A predictor network attempts to predict its next time step.'''
+
+    @property
+    def cost(self):
+        err = self.x[1:] - self.y[:-1]
+        return TT.mean((err * err).sum(axis=1)[self.error_start:])
+
+
 class Regressor(Network):
     '''A regressor attempts to produce a target output.'''
 
     def __init__(self, *args, **kwargs):
         super(Regressor, self).__init__(*args, **kwargs)
-        self.k = TT.matrix('k')
+        self.k = TT.tensor3('k')
 
     @property
     def inputs(self):

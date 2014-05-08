45da66ef69e6a2c40ad0c3450691e63800616f14
Leif Johnson <leif@leifjohnson.net>
2014-05-08 11:36:42 -0500

    Increase default training parameters -- number of updates and patience.

--- theanets/flags.py
+++ theanets/flags.py
@@ -38,9 +38,9 @@ g.add_argument('-O', '--optimize', default=(), nargs='+', metavar='[hf|cg|sgd|sa
                help='train with the given optimization method(s)')
 g.add_argument('--no-learn-biases', action='store_true',
                help='if set, do not update bias parameters during learning')
-g.add_argument('-u', '--num-updates', type=int, default=1024, metavar='N',
+g.add_argument('-u', '--num-updates', type=int, default=10000, metavar='N',
                help='perform at most N parameter updates')
-g.add_argument('-p', '--patience', type=int, default=30, metavar='N',
+g.add_argument('-p', '--patience', type=int, default=50, metavar='N',
                help='stop training if no improvement for N updates')
 g.add_argument('-v', '--validate', type=int, default=10, metavar='N',
                help='validate the model every N updates')

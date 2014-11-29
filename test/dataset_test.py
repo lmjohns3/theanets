7ee028f537586217ee959bc546a4846a47b53af5
Leif Johnson <leif@leifjohnson.net>
2014-11-29 13:07:21 -0500

    Refactor dataset tests.

--- test/dataset_test.py
+++ test/dataset_test.py
@@ -3,30 +3,37 @@ import theanets
 
 
 class TestDataset:
-    def setUp(self):
-        self.dataset = theanets.dataset.Dataset(
-            np.arange(101)[:, None],
+    def test_iterate(self):
+        dataset = theanets.dataset.Dataset(
+            np.random.randn(101, 1),
             label='foo',
             batches=4,
             size=10,
         )
 
-    def test_setup(self):
-        assert self.dataset.label == 'foo'
-        assert len(self.dataset.batches) == 10
-        assert self.dataset.number_batches == 4
-
-    def test_iterate(self):
-        batches_ = list(self.dataset.batches)
+        batches_ = list(dataset.batches)
 
         # check we iterate over the correct number of batches.
-        assert sum(1 for _ in self.dataset) == 4
+        assert sum(1 for _ in dataset) == 4
 
         # check the dataset didn't get shuffled (yet).
-        assert all(a is b for a, b in zip(self.dataset.batches, batches_))
+        assert all(a is b for a, b in zip(dataset.batches, batches_))
 
-        assert sum(1 for _ in self.dataset) == 4
-        assert sum(1 for _ in self.dataset) == 4
+        assert sum(1 for _ in dataset) == 4
+        assert sum(1 for _ in dataset) == 4
 
         # check the dataset did get shuffled.
-        assert not all(a is b for a, b in zip(self.dataset.batches, batches_))
+        assert not all(a is b for a, b in zip(dataset.batches, batches_))
+
+    def test_threedee(self):
+        dataset = theanets.dataset.Dataset(
+            np.random.randn(101, 202, 3),
+            batches=4,
+            size=20,
+        )
+
+        i = 0
+        for x, in dataset:
+            assert x.shape == (101, 20, 3)
+            i += 1
+        assert i == 4

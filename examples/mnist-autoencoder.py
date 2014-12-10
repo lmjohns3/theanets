a8eb2f9c1f5b6b71e38a2c6877c10fd5503927e6
Leif Johnson <leif@leifjohnson.net>
2014-12-10 16:47:53 -0600

    Update autoencoder example to use climate flags.

--- examples/mnist-autoencoder.py
+++ examples/mnist-autoencoder.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python
 
+import climate
 import matplotlib.pyplot as plt
 import theanets
 
 from utils import load_mnist, plot_layers, plot_images
 
+g = climate.add_arg_group('MNIST Example')
+g.add_argument('--features', type=int, default=8, metavar='N',
+               help='train a model using N^2 hidden-layer features')
 
-train, valid, _ = load_mnist()
 
-N = 8
+def main(args):
+    train, valid, _ = load_mnist()
 
-e = theanets.Experiment(
-    theanets.Autoencoder,
-    layers=(784, N * N, 784),
-    train_batches=100,
-)
-e.train(train, valid)
+    e = theanets.Experiment(
+        theanets.Autoencoder,
+        layers=(784, args.features ** 2, 784))
 
-plot_layers(e.network.weights)
-plt.tight_layout()
-plt.show()
+    e.train(train, valid)
 
-valid = valid[:N*N]
-plot_images(valid, 121, 'Sample data')
-plot_images(e.network.predict(valid), 122, 'Reconstructed data')
-plt.tight_layout()
-plt.show()
+    plot_layers(e.network.weights)
+    plt.tight_layout()
+    plt.show()
+
+    v = valid[:100]
+    plot_images(v, 121, 'Sample data')
+    plot_images(e.network.predict(v), 122, 'Reconstructed data')
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    climate.call(main)

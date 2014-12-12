f6d3f635ff1ed2532a3834b6bf7056a638972c30
Leif Johnson <leif@leifjohnson.net>
2014-12-12 14:27:26 -0600

    Add an example using the cifar10 dataset.

---
+++ examples/cifar-autoencoder.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import climate
+import matplotlib.pyplot as plt
+import theanets
+
+from utils import load_cifar, plot_layers, plot_images
+
+g = climate.add_arg_group('CIFAR Example')
+g.add_argument('--features', type=int, default=32, metavar='N',
+               help='train a model using N^2 hidden-layer features')
+
+
+def main(args):
+    train, valid, _ = load_cifar()
+
+    e = theanets.Experiment(
+        theanets.Autoencoder,
+        layers=(3072, args.features ** 2, 3072))
+
+    e.train(train, valid)
+
+    plot_layers(e.network.weights, channels=3)
+    plt.tight_layout()
+    plt.show()
+
+    valid = valid[:100]
+    plot_images(valid, 121, 'Sample data', channels=3)
+    plot_images(e.network.predict(valid), 122, 'Reconstructed data', channels=3)
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    climate.call(main)
--- examples/utils.py
+++ examples/utils.py
@@ -17,6 +17,7 @@ except ImportError:
 
 try:
     import skdata.mnist
+    import skdata.cifar10
 except ImportError:
     logging.critical('please install skdata to run the examples!')
     raise
@@ -29,9 +30,9 @@ def load_mnist(labels=False):
     def arr(n, dtype):
         arr = mnist.arrays[n]
         return arr.reshape((len(arr), -1)).astype(dtype)
-    train_images = arr('train_images', 'f') / 255.
+    train_images = arr('train_images', np.float32) / 255
     train_labels = arr('train_labels', np.uint8)
-    test_images = arr('test_images', 'f') / 255.
+    test_images = arr('test_images', np.float32) / 255
     test_labels = arr('test_labels', np.uint8)
     if labels:
         return ((train_images[:50000], train_labels[:50000, 0]),
@@ -40,35 +41,47 @@ def load_mnist(labels=False):
     return train_images[:50000], train_images[50000:], test_images
 
 
-def plot_images(imgs, loc, title=None):
+def load_cifar(labels=False):
+    cifar = skdata.cifar10.dataset.CIFAR10()
+    cifar.meta  # trigger download if needed.
+    pixels = cifar._pixels.astype(np.float32).reshape((len(cifar._pixels), -1)) / 255
+    if labels:
+        labels = cifar._labels.astype(np.uint8)
+        return ((pixels[:40000], labels[:40000, 0]),
+                (pixels[40000:50000], labels[40000:50000, 0]),
+                (pixels[50000:], labels[50000:, 0]))
+    return pixels[:40000], pixels[40000:50000], pixels[50000:]
+
+
+def plot_images(imgs, loc, title=None, channels=1):
     '''Plot an array of images.
 
-    We assume that we are given a matrix of data whose shape is (n*n, s*s) --
+    We assume that we are given a matrix of data whose shape is (n*n, s*s*c) --
     that is, there are n^2 images along the first axis of the array, and each
-    image is a square measuring s pixels on a side. Each row of the input will
+    image is c squares measuring s pixels on a side. Each row of the input will
     be plotted as a sub-region within a single image array containing an n x n
     grid of images.
     '''
     n = int(np.sqrt(len(imgs)))
     assert n * n == len(imgs), 'images array must contain a square number of rows!'
-    s = int(np.sqrt(len(imgs[0])))
-    assert s * s == len(imgs[0]), 'images must be square!'
+    s = int(np.sqrt(len(imgs[0]) / channels))
+    assert s * s == len(imgs[0]) / channels, 'images must be square!'
 
-    img = np.zeros((s * n, s * n), dtype=imgs[0].dtype)
+    img = np.zeros((s * n, s * n, channels), dtype=imgs[0].dtype)
     for i, pix in enumerate(imgs):
         r, c = divmod(i, n)
-        img[r * s:(r+1) * s, c * s:(c+1) * s] = pix.reshape((s, s))
+        img[r * s:(r+1) * s, c * s:(c+1) * s] = pix.reshape((s, s, channels))
 
     ax = plt.gcf().add_subplot(loc)
     ax.xaxis.set_visible(False)
     ax.yaxis.set_visible(False)
     ax.set_frame_on(False)
-    ax.imshow(img, cmap=plt.cm.gray)
+    ax.imshow(img.squeeze(), cmap=plt.cm.gray)
     if title:
         ax.set_title(title)
 
 
-def plot_layers(weights, tied_weights=False):
+def plot_layers(weights, tied_weights=False, channels=1):
     '''Create a plot of weights, visualized as "bottom-level" pixel arrays.'''
     if hasattr(weights[0], 'get_value'):
         weights = [w.get_value() for w in weights]
@@ -76,12 +89,22 @@ def plot_layers(weights, tied_weights=False):
     imgs = np.eye(weights[0].shape[0])
     for i, weight in enumerate(weights[:-1]):
         imgs = np.dot(weight.T, imgs)
-        plot_images(imgs, 100 + 10 * k + i + 1, 'Layer {}'.format(i+1))
+        plot_images(imgs,
+                    100 + 10 * k + i + 1,
+                    channels=channels,
+                    title='Layer {}'.format(i+1))
     weight = weights[-1]
-    if int(np.sqrt(weight.shape[1])) ** 2 != weight.shape[1]:
+    n = weight.shape[1] / channels
+    if int(np.sqrt(n)) ** 2 != n:
         return
     if tied_weights:
         imgs = np.dot(weight.T, imgs)
-        plot_images(imgs, 100 + 10 * k + k, 'Layer {}'.format(k))
+        plot_images(imgs,
+                    100 + 10 * k + k,
+                    channels=channels,
+                    title='Layer {}'.format(k))
     else:
-        plot_images(weight, 100 + 10 * k + k, 'Decoding weights')
+        plot_images(weight,
+                    100 + 10 * k + k,
+                    channels=channels,
+                    title='Decoding weights')

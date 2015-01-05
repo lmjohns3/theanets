8a3196ec46517def4eded8cd62f94adcdd302c1c
Leif Johnson <leif@lmjohns3.com>
2015-01-05 11:58:32 -0600

    Add more layer tests.

--- test/layers_test.py
+++ test/layers_test.py
@@ -1,40 +1,113 @@
-import theanets
 import numpy as np
+import theanets
+import theano.tensor as TT
 
 
 class TestLayer:
     def test_build(self):
-        layer = theanets.layers.build('feedforward', nin=2, nout=4)
-        assert isinstance(layer, theanets.layers.Layer)
+        for f in 'feedforward Feedforward classifier rnn lstm'.split():
+            l = theanets.layers.build(f, nin=2, nout=4)
+            assert isinstance(l, theanets.layers.Layer)
 
 
-class TestFeedforward:
+class Base:
+    def setUp(self):
+        self.x = TT.matrix('x')
+
+
+class TestFeedforward(Base):
+    def _build(self):
+        return theanets.layers.Feedforward(nin=2, nout=4)
+
     def test_create(self):
-        l = theanets.layers.Feedforward(nin=2, nout=4)
+        l = self._build()
         assert l.reset() == 12
 
-class TestTied:
-    def test_create(self):
+    def test_transform(self):
+        l = self._build()
+        l.reset()
+        out, upd = l.transform(self.x)
+        assert out is not None
+        assert not upd
+
+
+class TestTied(Base):
+    def _build(self):
         l0 = theanets.layers.Feedforward(nin=2, nout=4)
-        l = theanets.layers.Tied(partner=l0)
+        l0.reset()
+        return theanets.layers.Tied(partner=l0)
+
+    def test_create(self):
+        l = self._build()
         assert l.reset() == 2
 
-class TestClassifier:
+    def test_transform(self):
+        l = self._build()
+        l.reset()
+        out, upd = l.transform(self.x)
+        assert out is not None
+        assert not upd
+
+
+class TestClassifier(Base):
+    def _build(self):
+        return theanets.layers.Classifier(nin=2, nout=4)
+
     def test_create(self):
-        l = theanets.layers.Classifier(nin=2, nout=4)
+        l = self._build()
         assert l.reset() == 12
 
-class TestRNN:
+    def test_transform(self):
+        l = self._build()
+        l.reset()
+        out, upd = l.transform(self.x)
+        assert out is not None
+        assert not upd
+
+
+class TestRNN(Base):
+    def _build(self):
+        return theanets.layers.RNN(nin=2, nout=4)
+
     def test_create(self):
-        l = theanets.layers.RNN(nin=2, nout=4)
+        l = self._build()
         assert l.reset() == 28
 
-class TestMRNN:
+    def test_transform(self):
+        l = self._build()
+        l.reset()
+        out, upd = l.transform(self.x)
+        assert out is not None
+        assert not upd
+
+
+class TestMRNN(Base):
+    def _build(self):
+        return theanets.layers.MRNN(nin=2, nout=4, factors=3)
+
     def test_create(self):
-        l = theanets.layers.MRNN(nin=2, nout=4, factors=3)
+        l = self._build()
         assert l.reset() == 42
 
-class TestLSTM:
+    def test_transform(self):
+        l = self._build()
+        l.reset()
+        out, upd = l.transform(self.x)
+        assert out is not None
+        assert not upd
+
+
+class TestLSTM(Base):
+    def _build(self):
+        return theanets.layers.LSTM(nin=2, nout=4)
+
     def test_create(self):
-        l = theanets.layers.LSTM(nin=2, nout=4)
+        l = self._build()
         assert l.reset() == 124
+
+    def test_transform(self):
+        l = self._build()
+        l.reset()
+        out, upd = l.transform(self.x)
+        assert out is not None
+        assert not upd

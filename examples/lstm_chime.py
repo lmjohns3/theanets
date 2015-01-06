ede0f91435871e656d36d08a3cf068db1693abcd
Hakan Erdogan <erdogan@merl.com>
2015-01-06 17:29:23 -0500

    lstm example get data online

--- examples/lstm_chime.py
+++ examples/lstm_chime.py
@@ -5,19 +5,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 import theanets
 import scipy.io
+import os.path
+
+from StringIO import StringIO
+from zipfile import ZipFile
+from urllib import urlopen
 
 climate.enable_default_logging()
-logging = climate.get_logger(__name__)
+logging = climate.get_logger('lstm_chime_example')
 
-# do fixed segments for now (warning: not corresponding to real utterances!)
+# do fixed segments for now (warning: each segment does not correspond to real utterances!)
 SEQLEN = 100
 BATCH_SIZE = 50
 
 BATCH_STEP = BATCH_SIZE * SEQLEN
 
-# get these files from the repository at https://github.com/craffel/lstm_benchmarks
-TRAIN_NC = '../data/train_1_speaker.nc'
-VAL_NC = '../data/val_1_speaker.nc'
+TRAIN_NC = '.chime1_data/train_1_speaker.nc'
+VAL_NC = '.chime1_data/val_1_speaker.nc'
+
+if not os.path.isfile(TRAIN_NC) or not os.path.isfile(VAL_NC):
+    if not os.path.exists(os.path.dirname(TRAIN_NC)):
+        os.makedirs(os.path.dirname(TRAIN_NC))
+    # get the data files from the repository at https://github.com/craffel/lstm_benchmarks
+    zipurl="https://github.com/craffel/lstm_benchmarks/archive/master.zip"
+    logging.info('attempting data copy from url: %s', zipurl)
+    url = urlopen(zipurl)
+    z = ZipFile(StringIO(url.read()))
+    data=z.read('lstm_benchmarks-master/data/train_1_speaker.nc')
+    savefile = open(TRAIN_NC, "wb")
+    savefile.write(data)
+    savefile.close()
+    data=z.read('lstm_benchmarks-master/data/val_1_speaker.nc')
+    savefile = open(VAL_NC, "wb")
+    savefile.write(data)
+    savefile.close()
+    data=None
+    z.close()
+    logging.info("Done (hopefully).")
 
 with open(TRAIN_NC, 'r') as f:
     train_data = scipy.io.netcdf_file(f)
@@ -45,7 +69,8 @@ e = theanets.Experiment(
     output_activation='softmax',
     hidden_activation='tanh',
     input_noise=0.5,
-    batch_size=BATCH_SIZE)
+    batch_size=BATCH_SIZE,
+    train_batches=BATCH_SIZE)
 
 def get_batch(V, variable, batch, dim, seqlen=SEQLEN, batch_size=BATCH_SIZE):
     '''
@@ -53,7 +78,7 @@ def get_batch(V, variable, batch, dim, seqlen=SEQLEN, batch_size=BATCH_SIZE):
         
         :parameters:
         - V : scipy.io.netcdf_file.variables object
-        netcdf file variables
+        scipy.io.netcdf_file.variables object to read variables from the netcdf file
         - variable : string
         variable name in the netcdf file
         - batch : int
@@ -86,10 +111,12 @@ def get_batch(V, variable, batch, dim, seqlen=SEQLEN, batch_size=BATCH_SIZE):
                     X_batch[t, n] = V[variable].data[batch*(batch_size*seqlen)+n*seqlen+t].astype('int32')
     return X_batch
 
-# each time, we go to the file and read it from there. I want this to be generalizable to larger size files
-# which may not fit in the memory, so I avoid reading the whole file at once
+# each time, we read data from the netcdf file. 
+# I want the code to be generalizable to larger size files
+# which may not fit in the memory, 
+# so I avoid reading the whole file at once
 def generate():
-    i = np.random.choice(nbatch_train)
+    i = np.random.choice(nbatch_train)  # random batches for now, should change to sample without replacement later
     s = get_batch(trainV,'inputs',i,XDIM)
     t = get_batch(trainV,'targetClasses',i,1)
     return [s, t]
@@ -100,7 +127,6 @@ y_vals=np.zeros((nbatch_val, SEQLEN, BATCH_SIZE ), dtype='int32')
 for i in range(nbatch_val):
     X_vals[i]=get_batch(valV,'inputs',i,XDIM)
     y_vals[i]=get_batch(valV,'targetClasses',i,1)
-#n_val=nbatch_val*BATCH_STEP
 
 # get a random batch to check sizes
 [tr_x,tr_y]= generate()
@@ -108,7 +134,7 @@ for i in range(nbatch_val):
 logging.info('train data each batch: %s -> %s', tr_x.shape, tr_y.shape)
 logging.info('validation data all batches: %s -> %s', X_vals.shape, y_vals.shape)
 
-# evaluate on the first batch of validation data only (for now)
+# evaluate performance on the first batch of the validation data only (for now)
 e.train(generate, (X_vals[0], y_vals[0]), momentum=0.99)
 #e.train(generate, momentum=0.99)
 

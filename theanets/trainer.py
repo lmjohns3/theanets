b8ae49498b0e1ed87647aa647943c0ff6de49802
Leif Johnson <leif@lmjohns3.com>
2015-01-13 12:00:34 -0600

    Clip gradients based on norm, not element-wise.

--- theanets/trainer.py
+++ theanets/trainer.py
@@ -185,11 +185,16 @@ class SGD(Trainer):
 
     def learning_updates(self):
         for param in self.params:
-            grad = TT.grad(self.J, param).clip(-self.clip, self.clip)
+            grad = self.clipped_grad(param)
             vel_tm1 = self.shared_like(param, 'vel')
             vel_t = self.momentum * vel_tm1 - self.learning_rate * grad
             yield vel_tm1, vel_t
             yield param, param + vel_t
+
+    def clipped_grad(self, param):
+        grad = TT.grad(self.J, param)
+        norm = TT.sqrt((grad * grad).sum())
+        return grad * TT.minimum(1, self.clip / norm)
 
     @staticmethod
     def shared_like(param, name, init=0):
@@ -313,7 +318,7 @@ class NAG(SGD):
     def learning_updates(self):
         # step 2. record the gradient here.
         for param, step, vel in zip(self.params, self._steps, self._vels):
-            grad = TT.grad(self.J, param).clip(-self.clip, self.clip)
+            grad = self.clipped_grad(param)
             yield vel, step - self.learning_rate * grad
 
         # step 3. update each of the parameters, removing the step that we took
@@ -362,7 +367,7 @@ class Rprop(SGD):
         for param in self.params:
             grad_tm1 = self.shared_like(param, 'grad')
             step_tm1 = self.shared_like(param, 'step', self.learning_rate)
-            grad = TT.grad(self.J, param).clip(-self.clip, self.clip)
+            grad = self.clipped_grad(param)
             test = grad * grad_tm1
             same = TT.gt(test, 0)
             diff = TT.lt(test, 0)
@@ -410,7 +415,7 @@ class RmsProp(SGD):
             g1_tm1 = self.shared_like(param, 'g1_ewma')
             g2_tm1 = self.shared_like(param, 'g2_ewma')
             vel_tm1 = self.shared_like(param, 'vel')
-            grad = TT.grad(self.J, param).clip(-self.clip, self.clip)
+            grad = self.clipped_grad(param)
             g1_t = self.ewma * g1_tm1 + (1 - self.ewma) * grad
             g2_t = self.ewma * g2_tm1 + (1 - self.ewma) * grad * grad
             rms = TT.sqrt(g2_t - g1_t * g1_t + 1e-4)
@@ -447,7 +452,7 @@ class ADADELTA(RmsProp):
             x2_tm1 = self.shared_like(param, 'x2_ewma')
             g2_tm1 = self.shared_like(param, 'g2_ewma')
             vel_tm1 = self.shared_like(param, 'vel')
-            grad = TT.grad(self.J, param).clip(-self.clip, self.clip)
+            grad = self.clipped_grad(param)
             g2_t = self.ewma * g2_tm1 + (1 - self.ewma) * grad * grad
             delta = grad * TT.sqrt(x2_tm1 + eps) / TT.sqrt(g2_t + eps)
             x2_t = self.ewma * x2_tm1 + (1 - self.ewma) * delta * delta

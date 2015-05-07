cd73e400e1ad85c07a8b4acc4341c4c022c4fe01
Leif Johnson <leif@lmjohns3.com>
2015-05-07 11:02:03 -0500

    Generate output sin/cos waves from no input.
    Also, use a clockwork layer for this example.

--- examples/recurrent-sinusoid.py
+++ examples/recurrent-sinusoid.py
@@ -4,47 +4,38 @@ import climate
 import logging
 import matplotlib.pyplot as plt
 import numpy as np
-import numpy.random as rng
 import theanets
 
 climate.enable_default_logging()
 
-BATCH_SIZE = 32
-T = np.linspace(0, 1, 100)
+TAU = 2 * np.pi
 
-concat = lambda a, b: np.concatenate([a, b], axis=2).astype('f')
-
-def sines():
-    f = np.exp(rng.uniform(0, 1, size=BATCH_SIZE))
-    time = np.outer(T, np.ones(BATCH_SIZE))[..., None]
-    freq = np.outer(np.ones_like(T), f)[..., None]
-    z = 2 * np.pi * freq * time
-    return concat(time, freq), concat(np.sin(z), np.cos(z))
-
-src, tgt = sines()
-logging.info('data batches: %s -> %s', src.shape, tgt.shape)
+BATCH_SIZE = 2
+T = np.linspace(0, TAU, 256)
+ZERO = np.zeros((len(T), BATCH_SIZE, 1), 'f')
+COEFFS = ((2, 1.5), (3, 1.8), (4, 1.1))
+SIN = sum(c * np.sin(TAU * f * T) for c, f in COEFFS)
+COS = sum(c * np.cos(TAU * f * T) for c, f in COEFFS)
+W = np.concatenate([SIN[..., None], COS[..., None]], axis=1)
+WAVES = np.concatenate([W[..., None], W[..., None]], axis=2).astype('f')
 
 e = theanets.Experiment(
     theanets.recurrent.Regressor,
-    layers=(2, ('rnn', 10), 2))
-e.train(sines, batch_size=BATCH_SIZE)
-prd = e.network.predict(src)
+    layers=(1, dict(form='clockwork', size=12, periods=(1, 2, 4)), 2))
 
-for i in range(3):
-    ax = plt.subplot(3, 1, i + 1)
-    ax.plot(tgt[:, i, 0].flatten(), '--', color='#1f77b4', label='Target Sine')
-    ax.plot(tgt[:, i, 1].flatten(), '--', color='#2ca02c', label='Target Cosine')
-    ax.plot(prd[:, i, 0].flatten(), '-', color='#1f77b4', lw=2, label='Predicted Sine')
-    ax.plot(prd[:, i, 1].flatten(), '-', color='#2ca02c', lw=2, label='Predicted Cosine')
-    ax.xaxis.tick_bottom()
-    ax.yaxis.tick_left()
-    ax.spines['top'].set_color('none')
-    ax.spines['right'].set_color('none')
-    ax.set_ylabel('Amplitude')
-    if i == 0:
-        plt.legend()
-    if i == 2:
-        ax.set_xlabel('Time')
-    else:
-        ax.set_xticks([])
+e.train([ZERO, WAVES], batch_size=BATCH_SIZE)
+
+prd = e.network.predict(ZERO)
+ax = plt.subplot(111)
+ax.plot(T, SIN, ':', color='#1f77b4', lw=2, label='Target Sine')
+ax.plot(T, COS, ':', color='#2ca02c', lw=2, label='Target Cosine')
+ax.plot(T, prd[:, 0, 0].flatten(), '-', color='#1f77b4', lw=2, label='Predicted Sine')
+ax.plot(T, prd[:, 0, 1].flatten(), '-', color='#2ca02c', lw=2, label='Predicted Cosine')
+ax.xaxis.tick_bottom()
+ax.yaxis.tick_left()
+ax.spines['top'].set_color('none')
+ax.spines['right'].set_color('none')
+ax.set_ylabel('Amplitude')
+ax.set_xlabel('Time')
+plt.legend()
 plt.show()

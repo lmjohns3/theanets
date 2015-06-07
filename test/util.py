447f19638c43cf273b6922796a203d33407bc29e
Leif Johnson <leif@lmjohns3.com>
2015-06-07 13:40:30 -0500

    Use proper namespace for constants.

--- test/util.py
+++ test/util.py
@@ -10,5 +10,5 @@ class MNIST(object):
     def setUp(self):
         # we just create some random "mnist digit" data of the right shape.
         np.random.seed(3)
-        self.images = np.random.randn(NUM_DIGITS, DIGIT_SIZE).astype('f')
-        self.labels = np.random.randint(0, 10, NUM_DIGITS).astype('i')
+        self.images = np.random.randn(MNIST.NUM_DIGITS, MNIST.DIGIT_SIZE).astype('f')
+        self.labels = np.random.randint(0, 10, MNIST.NUM_DIGITS).astype('i')

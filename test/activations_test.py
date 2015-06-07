98f278baeff2ee01cfc301ea7ea3f5db614ff85c
Leif Johnson <leif@lmjohns3.com>
2015-06-07 16:22:02 -0500

    Reindent for pep8.

--- test/activations_test.py
+++ test/activations_test.py
@@ -3,13 +3,15 @@ import theanets
 
 class TestBuild:
     def test_build(self):
-        a = theanets.layers.Feedforward(inputs=2, size=3, activation='relu').activate
+        a = theanets.layers.Feedforward(
+            inputs=2, size=3, activation='relu').activate
         assert callable(a)
         assert a.name == 'relu'
         assert a.params == []
 
     def test_build_composed(self):
-        a = theanets.layers.Feedforward(inputs=2, size=3, activation='relu+norm:z').activate
+        a = theanets.layers.Feedforward(
+            inputs=2, size=3, activation='relu+norm:z').activate
         assert callable(a)
         assert a.name == 'norm:z(relu)', a.name
         assert a.params == []
@@ -17,16 +19,19 @@ class TestBuild:
 
 class TestParametric:
     def test_prelu(self):
-        l = theanets.layers.Feedforward(inputs=2, size=3, activation='prelu', name='l')
+        l = theanets.layers.Feedforward(
+            inputs=2, size=3, activation='prelu', name='l')
         actual = sorted(p.name for p in l.params)
         assert actual == ['l.b', 'l.leak', 'l.w'], actual
 
     def test_lgrelu(self):
-        l = theanets.layers.Feedforward(inputs=2, size=3, activation='lgrelu', name='l')
+        l = theanets.layers.Feedforward(
+            inputs=2, size=3, activation='lgrelu', name='l')
         actual = sorted(p.name for p in l.params)
         assert actual == ['l.b', 'l.gain', 'l.leak', 'l.w'], actual
 
     def test_maxout(self):
-        l = theanets.layers.Feedforward(inputs=2, size=3, activation='maxout:8', name='l')
+        l = theanets.layers.Feedforward(
+            inputs=2, size=3, activation='maxout:8', name='l')
         actual = sorted(p.name for p in l.params)
         assert actual == ['l.b', 'l.intercept', 'l.slope', 'l.w'], actual

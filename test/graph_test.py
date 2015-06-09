1e8bec93016dbb016688360c73c39c88d96dde40
Leif Johnson <leif@lmjohns3.com>
2015-06-09 15:26:43 -0500

    Move tests for Regressor to feedforward_test.py.

--- test/graph_test.py
+++ test/graph_test.py
@@ -5,34 +5,9 @@ import util
 
 
 class TestNetwork(util.MNIST):
-    def _build(self, *hiddens):
-        return theanets.Regressor((self.DIGIT_SIZE, ) + hiddens)
-
-    def test_predict(self):
-        net = self._build(15, 13)
-        y = net.predict(self.images)
-        assert y.shape == (self.NUM_DIGITS, 13)
-
-    def test_feed_forward(self):
-        net = self._build(15, 13)
-        hs = net.feed_forward(self.images)
-        assert len(hs) == 7, 'got {}'.format(list(hs.keys()))
-        assert hs['in:out'].shape == (self.NUM_DIGITS, self.DIGIT_SIZE)
-        assert hs['hid1:out'].shape == (self.NUM_DIGITS, 15)
-        assert hs['out:out'].shape == (self.NUM_DIGITS, 13)
-
-    def test_decode_from_multiple_layers(self):
-        net = self._build(13, 14, dict(
-            size=15, inputs={'hid2:out': 14, 'hid1:out': 13}))
-        hs = net.feed_forward(self.images)
-        assert len(hs) == 9, 'got {}'.format(list(hs.keys()))
-        assert hs['in:out'].shape == (self.NUM_DIGITS, self.DIGIT_SIZE)
-        assert hs['hid1:out'].shape == (self.NUM_DIGITS, 13)
-        assert hs['hid2:out'].shape == (self.NUM_DIGITS, 14)
-        assert hs['out:out'].shape == (self.NUM_DIGITS, 15)
-
     def test_updates(self):
-        assert not self._build(13).updates()
+        m = theanets.Regressor((self.DIGIT_SIZE, 13))
+        assert not m.updates()
 
     def test_layer_ints(self):
         m = theanets.Regressor((1, 2, 3))

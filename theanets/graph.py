13dfe6efff151cbfc04c250933e2cacb5f531b18
Quanzeng You <qyou@cs.rochester.edu>
2015-07-16 01:28:34 -0400

    shorten line length to pass the test

--- theanets/graph.py
+++ theanets/graph.py
@@ -366,8 +366,8 @@ class Network(object):
         def add(s):
             h.update(str(s).encode('utf-8'))
         h = hashlib.md5()
-        # Use ordereddict to avoid creating different graphs with the save kwargs but different orders. 
-        # See discussion at https://groups.google.com/forum/#!topic/theanets/nL6Nis29B7Q 
+        # See discussions
+        # https://groups.google.com/forum/#!topic/theanets/nL6Nis29B7Q 
         add(sorted(kwargs.items(), key=lambda x: x[0]))
         for l in self.layers:
             add('{}{}{}'.format(l.__class__.__name__, l.name, l.size))

763309de347be8c1b5d06408b1fbc9cb68fb2a13
Leif Johnson <leif@lmjohns3.com>
2015-08-13 19:01:48 -0500

    Use util.Base as a base class in layers test.

--- test/layers_test.py
+++ test/layers_test.py
@@ -2,21 +2,20 @@ import numpy as np
 import theanets
 import theano.tensor as TT
 
+import util
 
-class Base(object):
-    BATCH = 8
-    INPUTS = 2
-    SIZE = 4
-    OUTPUTS = 3
+
+class Base(util.Base):
+    NUM_HIDDEN = 4  # this needs to be an even integer for concatenate test.
 
     def setUp(self):
         self.x = TT.matrix('x')
         self.l = self._build()
 
     def test_feed_forward(self):
-        net = theanets.Regressor((Base.INPUTS, self.l, Base.OUTPUTS))
-        out = net.predict(np.random.randn(Base.BATCH, Base.INPUTS).astype('f'))
-        assert out.shape == (Base.BATCH, Base.OUTPUTS)
+        net = theanets.Regressor((self.NUM_INPUTS, self.l, self.NUM_OUTPUTS))
+        out = net.predict(self.INPUTS)
+        assert out.shape == (self.NUM_EXAMPLES, self.NUM_OUTPUTS)
 
     def assert_param_names(self, expected):
         if expected and not expected[0].startswith('l'):
@@ -45,11 +44,11 @@ class Base(object):
 class TestLayer(Base):
     def _build(self):
         return theanets.layers.Feedforward(
-            inputs=Base.INPUTS, size=Base.SIZE, name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, name='l')
 
     def test_build(self):
         for f in 'feedforward Feedforward classifier prod rnn lstm'.split():
-            l = theanets.Layer.build(f, inputs=Base.INPUTS, size=Base.SIZE)
+            l = theanets.Layer.build(f, inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN)
             assert isinstance(l, theanets.layers.Layer)
 
     def test_connect(self):
@@ -61,11 +60,11 @@ class TestLayer(Base):
 class TestFeedforward(Base):
     def _build(self):
         return theanets.layers.Feedforward(
-            inputs=Base.INPUTS, size=Base.SIZE, name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, name='l')
 
     def test_create(self):
         self.assert_param_names(['w', 'b'])
-        self.assert_count(12)
+        self.assert_count((1 + self.NUM_INPUTS) * self.NUM_HIDDEN)
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -76,22 +75,22 @@ class TestFeedforward(Base):
 class TestMultiFeedforward(Base):
     def _build(self):
         self.a = theanets.layers.Feedforward(
-            inputs={'in:out': Base.INPUTS}, size=Base.SIZE, name='a')
+            inputs={'in:out': self.NUM_INPUTS}, size=self.NUM_HIDDEN, name='a')
         self.b = theanets.layers.Feedforward(
-            inputs={'in:out': Base.INPUTS}, size=Base.SIZE, name='b')
+            inputs={'in:out': self.NUM_INPUTS}, size=self.NUM_HIDDEN, name='b')
         return theanets.layers.Feedforward(
-            inputs={'a:out': Base.SIZE, 'b:out': Base.SIZE},
-            size=Base.SIZE, name='l')
+            inputs={'a:out': self.NUM_HIDDEN, 'b:out': self.NUM_HIDDEN},
+            size=self.NUM_HIDDEN, name='l')
 
     def test_feed_forward(self):
         net = theanets.Regressor(
-            (Base.INPUTS, self.a, self.b, self.l, Base.OUTPUTS))
-        out = net.predict(np.random.randn(Base.BATCH, Base.INPUTS).astype('f'))
-        assert out.shape == (Base.BATCH, Base.OUTPUTS)
+            (self.NUM_INPUTS, self.a, self.b, self.l, self.NUM_OUTPUTS))
+        out = net.predict(self.INPUTS)
+        assert out.shape == (self.NUM_EXAMPLES, self.NUM_OUTPUTS)
 
     def test_create(self):
         self.assert_param_names(['w_a:out', 'w_b:out', 'b'])
-        self.assert_count(36)
+        self.assert_count((1 + 2 * self.NUM_HIDDEN) * self.NUM_HIDDEN)
 
     def test_transform(self):
         out, upd = self.l.transform({'a:out': self.x, 'b:out': self.x})
@@ -102,18 +101,18 @@ class TestMultiFeedforward(Base):
 class TestProduct(Base):
     def _build(self):
         self.a = theanets.layers.Feedforward(
-            inputs={'in:out': Base.INPUTS}, size=Base.SIZE, name='a')
+            inputs={'in:out': self.NUM_INPUTS}, size=self.NUM_HIDDEN, name='a')
         self.b = theanets.layers.Feedforward(
-            inputs={'in:out': Base.INPUTS}, size=Base.SIZE, name='b')
+            inputs={'in:out': self.NUM_INPUTS}, size=self.NUM_HIDDEN, name='b')
         return theanets.layers.Product(
-            inputs={'a:out': Base.SIZE, 'b:out': Base.SIZE},
-            size=Base.SIZE, name='l')
+            inputs={'a:out': self.NUM_HIDDEN, 'b:out': self.NUM_HIDDEN},
+            size=self.NUM_HIDDEN, name='l')
 
     def test_feed_forward(self):
         net = theanets.Regressor(
-            (Base.INPUTS, self.a, self.b, self.l, Base.OUTPUTS))
-        out = net.predict(np.random.randn(Base.BATCH, Base.INPUTS).astype('f'))
-        assert out.shape == (Base.BATCH, Base.OUTPUTS)
+            (self.NUM_INPUTS, self.a, self.b, self.l, self.NUM_OUTPUTS))
+        out = net.predict(self.INPUTS)
+        assert out.shape == (self.NUM_EXAMPLES, self.NUM_OUTPUTS)
 
     def test_create(self):
         self.assert_param_names([])
@@ -128,20 +127,20 @@ class TestProduct(Base):
 class TestConcatenate(Base):
     def _build(self):
         self.a = theanets.layers.Feedforward(
-            inputs={'in:out': Base.INPUTS}, size=Base.SIZE // 2, name='a')
+            inputs={'in:out': self.NUM_INPUTS}, size=self.NUM_HIDDEN // 2, name='a')
         self.b = theanets.layers.Feedforward(
-            inputs={'in:out': Base.INPUTS}, size=Base.SIZE // 2, name='b')
+            inputs={'in:out': self.NUM_INPUTS}, size=self.NUM_HIDDEN // 2, name='b')
         return theanets.layers.Concatenate(
-            inputs={'a:out': Base.SIZE // 2, 'b:out': Base.SIZE // 2},
-            size=Base.SIZE, name='l')
+            inputs={'a:out': self.NUM_HIDDEN // 2, 'b:out': self.NUM_HIDDEN // 2},
+            size=self.NUM_HIDDEN, name='l')
 
     def test_feed_forward(self):
         net = theanets.Regressor(
-            (Base.INPUTS, self.a, self.b, self.l, Base.OUTPUTS))
+            (self.NUM_INPUTS, self.a, self.b, self.l, self.NUM_OUTPUTS))
         for l in net.layers:
             print(l.name, l.__class__, l.inputs, l.size)
-        out = net.predict(np.random.randn(Base.BATCH, Base.INPUTS).astype('f'))
-        assert out.shape == (Base.BATCH, Base.OUTPUTS)
+        out = net.predict(self.INPUTS)
+        assert out.shape == (self.NUM_EXAMPLES, self.NUM_OUTPUTS)
 
     def test_create(self):
         self.assert_param_names([])
@@ -156,18 +155,18 @@ class TestConcatenate(Base):
 class TestTied(Base):
     def _build(self):
         self.l0 = theanets.layers.Feedforward(
-            inputs=Base.INPUTS, size=Base.SIZE, name='l0')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, name='l0')
         return theanets.layers.Tied(partner=self.l0, name='l')
 
     def test_feed_forward(self):
-        net = theanets.Autoencoder((Base.INPUTS, self.l0, self.l))
-        out = net.predict(np.random.randn(Base.BATCH, Base.INPUTS).astype('f'))
-        assert out.shape == (Base.BATCH, Base.INPUTS)
+        net = theanets.Autoencoder((self.NUM_INPUTS, self.l0, self.l))
+        out = net.predict(self.INPUTS)
+        assert out.shape == (self.NUM_EXAMPLES, self.NUM_INPUTS)
 
     def test_create(self):
         l = self._build()
         assert sorted(p.name for p in l.params) == [l.name + '.b']
-        self.assert_count(2)
+        self.assert_count(self.NUM_INPUTS)
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -178,11 +177,16 @@ class TestTied(Base):
 class TestClassifier(Base):
     def _build(self):
         return theanets.layers.Classifier(
-            inputs=Base.INPUTS, size=Base.SIZE, name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_CLASSES, name='l')
+
+    def test_feed_forward(self):
+        net = theanets.Classifier((self.NUM_INPUTS, self.l))
+        out = net.predict_proba(self.INPUTS)
+        assert out.shape == (self.NUM_EXAMPLES, self.NUM_CLASSES)
 
     def test_create(self):
         self.assert_param_names(['w', 'b'])
-        self.assert_count(12)
+        self.assert_count((1 + self.NUM_INPUTS) * self.NUM_CLASSES)
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -191,24 +195,28 @@ class TestClassifier(Base):
 
 
 class BaseRecurrent(Base):
-    TIMES = 12
+    NUM_TIMES = 12
+
+    INPUTS = np.random.randn(Base.NUM_EXAMPLES, NUM_TIMES, Base.NUM_INPUTS).astype('f')
 
     def setUp(self):
         super(BaseRecurrent, self).setUp()
         self.x = TT.tensor3('x')
 
     def test_feed_forward(self):
-        net = theanets.recurrent.Regressor((Base.INPUTS, self.l, Base.OUTPUTS))
-        out = net.predict(np.random.randn(
-            Base.BATCH, BaseRecurrent.TIMES, Base.INPUTS).astype('f'))
-        assert out.shape == (Base.BATCH, BaseRecurrent.TIMES, Base.OUTPUTS)
+        net = theanets.recurrent.Regressor((self.NUM_INPUTS, self.l, self.NUM_OUTPUTS))
+        out = net.predict(self.INPUTS)
+        assert out.shape == (self.NUM_EXAMPLES, self.NUM_TIMES, self.NUM_OUTPUTS)
 
 
 class TestReshape(BaseRecurrent):
     def _build(self):
-        # we get an input that's (BATCH, TIMES, INPUTS): (8, 12, 2).
-        # we'll reshape to (BATCH, 6, SIZE): (8, 6, 4).
-        return theanets.layers.Reshape(inputs=Base.INPUTS, shape=(6, Base.SIZE))
+        # we get an input that's (BATCH, TIMES, INPUTS): (64, 12, 7).
+        # we'll reshape to (BATCH, 14, 6): (64, 14, 6).
+        #
+        # of course this doesn't make any sense for a real nn to do, it's just
+        # to test the reshaping operation.
+        return theanets.layers.Reshape(inputs=self.NUM_INPUTS, shape=(14, 6))
 
     def test_create(self):
         self.assert_param_names([])
@@ -220,21 +228,19 @@ class TestReshape(BaseRecurrent):
         assert not upd
 
     def test_spec(self):
-        self.assert_spec(shape=[6, Base.SIZE], size=Base.SIZE, form='reshape')
+        self.assert_spec(shape=[14, 6], size=6, form='reshape')
 
     def test_feed_forward(self):
-        net = theanets.recurrent.Regressor((Base.INPUTS, self.l, Base.OUTPUTS))
-        out = net.predict(np.random.randn(
-            Base.BATCH, BaseRecurrent.TIMES, Base.INPUTS).astype('f'))
-        assert out.shape == (Base.BATCH, 6, Base.OUTPUTS)
+        net = theanets.recurrent.Regressor((self.NUM_INPUTS, self.l, self.NUM_OUTPUTS))
+        out = net.predict(self.INPUTS)
+        assert out.shape == (self.NUM_EXAMPLES, 14, self.NUM_OUTPUTS)
 
 
 class TestFlatten(BaseRecurrent):
     def _build(self):
-        # we get an input that's (BATCH, TIMES, INPUTS): (8, 12, 2).
-        # this gets flattened to (BATCH, TIMES * INPUTS): (8, 24).
-        return theanets.layers.Flatten(
-            inputs=Base.INPUTS, size=BaseRecurrent.TIMES * Base.INPUTS)
+        # we get an input that's (BATCH, TIMES, INPUTS): (8, 12, 7).
+        # this gets flattened to (BATCH, TIMES * INPUTS): (8, 84).
+        return theanets.layers.Flatten(inputs=self.NUM_INPUTS, size=84)
 
     def test_create(self):
         self.assert_param_names([])
@@ -246,26 +252,24 @@ class TestFlatten(BaseRecurrent):
         assert not upd
 
     def test_feed_forward(self):
-        net = theanets.recurrent.Regressor((Base.INPUTS, self.l, Base.OUTPUTS))
-        out = net.predict(np.random.randn(
-            Base.BATCH, BaseRecurrent.TIMES, Base.INPUTS).astype('f'))
-        assert out.shape == (Base.BATCH, Base.OUTPUTS)
+        net = theanets.recurrent.Regressor((self.NUM_INPUTS, self.l, self.NUM_OUTPUTS))
+        out = net.predict(self.INPUTS)
+        assert out.shape == (self.NUM_EXAMPLES, self.NUM_OUTPUTS)
 
 
 class TestConv1(BaseRecurrent):
     def _build(self):
         return theanets.layers.Conv1(
-            inputs=Base.INPUTS, size=Base.SIZE, filter_size=3, name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, filter_size=3, name='l')
 
     def test_feed_forward(self):
-        net = theanets.recurrent.Regressor((Base.INPUTS, self.l, Base.OUTPUTS))
-        out = net.predict(np.random.randn(
-            Base.BATCH, BaseRecurrent.TIMES, Base.INPUTS).astype('f'))
-        assert out.shape == (Base.BATCH, BaseRecurrent.TIMES - 2, Base.OUTPUTS)
+        net = theanets.recurrent.Regressor((self.NUM_INPUTS, self.l, self.NUM_OUTPUTS))
+        out = net.predict(self.INPUTS)
+        assert out.shape == (self.NUM_EXAMPLES, self.NUM_TIMES - 2, self.NUM_OUTPUTS)
 
     def test_create(self):
         self.assert_param_names(['b', 'w'])
-        self.assert_count(28)
+        self.assert_count((1 + 3 * self.NUM_INPUTS) * self.NUM_HIDDEN)
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -276,11 +280,11 @@ class TestConv1(BaseRecurrent):
 class TestRNN(BaseRecurrent):
     def _build(self):
         return theanets.layers.RNN(
-            inputs=Base.INPUTS, size=Base.SIZE, name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, name='l')
 
     def test_create(self):
         self.assert_param_names(['b', 'hh', 'xh'])
-        self.assert_count(28)
+        self.assert_count((1 + self.NUM_HIDDEN + self.NUM_INPUTS) * self.NUM_HIDDEN)
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -291,11 +295,12 @@ class TestRNN(BaseRecurrent):
 class TestARRNN(BaseRecurrent):
     def _build(self):
         return theanets.layers.ARRNN(
-            inputs=Base.INPUTS, size=Base.SIZE, name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, name='l')
 
     def test_create(self):
         self.assert_param_names(['b', 'hh', 'r', 'xh', 'xr'])
-        self.assert_count(40)
+        self.assert_count(
+            (1 + 1 + self.NUM_HIDDEN + 2 * self.NUM_INPUTS) * self.NUM_HIDDEN)
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -306,11 +311,12 @@ class TestARRNN(BaseRecurrent):
 class TestLRRNN(BaseRecurrent):
     def _build(self):
         return theanets.layers.LRRNN(
-            inputs=Base.INPUTS, size=Base.SIZE, name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, name='l')
 
     def test_create(self):
         self.assert_param_names(['b', 'hh', 'r', 'xh'])
-        self.assert_count(32)
+        self.assert_count(
+            (1 + 1 + self.NUM_INPUTS + self.NUM_HIDDEN) * self.NUM_HIDDEN)
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -321,11 +327,12 @@ class TestLRRNN(BaseRecurrent):
 class TestMRNN(BaseRecurrent):
     def _build(self):
         return theanets.layers.MRNN(
-            inputs=Base.INPUTS, size=Base.SIZE, factors=3, name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, factors=3, name='l')
 
     def test_create(self):
         self.assert_param_names(['b', 'fh', 'hf', 'xf', 'xh'])
-        self.assert_count(42)
+        self.assert_count(
+            (1 + 3 + 3 + self.NUM_INPUTS) * self.NUM_HIDDEN + 3 * self.NUM_INPUTS)
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -336,11 +343,12 @@ class TestMRNN(BaseRecurrent):
 class TestLSTM(BaseRecurrent):
     def _build(self):
         return theanets.layers.LSTM(
-            inputs=Base.INPUTS, size=Base.SIZE, name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, name='l')
 
     def test_create(self):
         self.assert_param_names(['b', 'cf', 'ci', 'co', 'hh', 'xh'])
-        self.assert_count(124)
+        self.assert_count(
+            (4 + 3 + 4 * self.NUM_HIDDEN + 4 * self.NUM_INPUTS) * self.NUM_HIDDEN)
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -351,13 +359,14 @@ class TestLSTM(BaseRecurrent):
 class TestGRU(BaseRecurrent):
     def _build(self):
         return theanets.layers.GRU(
-            inputs=Base.INPUTS, size=Base.SIZE, name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, name='l')
 
     def test_create(self):
         self.assert_param_names(['bh', 'br', 'bz',
                                  'hh', 'hr', 'hz',
                                  'xh', 'xr', 'xz'])
-        self.assert_count(84)
+        self.assert_count(
+            3 * (1 + self.NUM_INPUTS + self.NUM_HIDDEN) * self.NUM_HIDDEN)
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -368,13 +377,14 @@ class TestGRU(BaseRecurrent):
 class TestMUT1(BaseRecurrent):
     def _build(self):
         return theanets.layers.MUT1(
-            inputs=Base.INPUTS, size=Base.SIZE, name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, name='l')
 
     def test_create(self):
         self.assert_param_names(['bh', 'br', 'bz',
                                  'hh', 'hr',
                                  'xh', 'xr', 'xz'])
-        self.assert_count(68)
+        self.assert_count(
+            (3 + 3 * self.NUM_INPUTS + 2 * self.NUM_HIDDEN) * self.NUM_HIDDEN)
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -385,11 +395,11 @@ class TestMUT1(BaseRecurrent):
 class TestClockwork(BaseRecurrent):
     def _build(self):
         return theanets.layers.Clockwork(
-            inputs=Base.INPUTS, size=Base.SIZE, periods=(2, 5), name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, periods=(2, 5), name='l')
 
     def test_create(self):
         self.assert_param_names(['b', 'xh', 'hh'])
-        self.assert_count(28)
+        self.assert_count((1 + self.NUM_INPUTS + self.NUM_HIDDEN) * self.NUM_HIDDEN)
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -397,19 +407,20 @@ class TestClockwork(BaseRecurrent):
         assert not upd
 
     def test_spec(self):
-        self.assert_spec(periods=(5, 2), size=Base.SIZE, form='clockwork')
+        self.assert_spec(periods=(5, 2), size=self.NUM_HIDDEN, form='clockwork')
 
 
 class TestBidirectional(BaseRecurrent):
     def _build(self):
         return theanets.layers.Bidirectional(
-            inputs=Base.INPUTS, size=Base.SIZE, worker='arrnn', name='l')
+            inputs=self.NUM_INPUTS, size=self.NUM_HIDDEN, worker='arrnn', name='l')
 
     def test_create(self):
         self.assert_param_names(
             ['l_bw.b', 'l_bw.hh', 'l_bw.r', 'l_bw.xh', 'l_bw.xr',
              'l_fw.b', 'l_fw.hh', 'l_fw.r', 'l_fw.xh', 'l_fw.xr'])
-        self.assert_count(32)
+        h = self.NUM_HIDDEN // 2
+        self.assert_count(2 * h * (2 + h + 2 * self.NUM_INPUTS))
 
     def test_transform(self):
         out, upd = self.l.transform(dict(out=self.x))
@@ -417,4 +428,4 @@ class TestBidirectional(BaseRecurrent):
         assert not upd
 
     def test_spec(self):
-        self.assert_spec(size=Base.SIZE, form='bidirectional', worker='arrnn')
+        self.assert_spec(size=self.NUM_HIDDEN, form='bidirectional', worker='arrnn')

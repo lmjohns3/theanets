dce5b5750eca9e16fa192fbdd7e442e536cbb35a
Leif Johnson <leif@lmjohns3.com>
2016-02-13 23:57:50 -0600

    Refactor tests.

--- test/regularizers_test.py
+++ test/regularizers_test.py
@@ -1,98 +1,86 @@
+import climate
 import pytest
 import theanets
 
-import util
+import util as u
+
+climate.enable_default_logging()
 
 
-class Mixin(object):
+@pytest.fixture
+def exp():
+    return theanets.Regressor([u.NUM_INPUTS, 20, u.NUM_OUTPUTS], rng=115)
+
+
+def assert_progress(net, **kwargs):
+    start = best = None
+    for _, val in net.itertrain(
+            [u.INPUTS, u.OUTPUTS],
+            algorithm='sgd',
+            patience=2,
+            min_improvement=0.01,
+            max_gradient_norm=1,
+            batch_size=u.NUM_EXAMPLES,
+            **kwargs):
+        if start is None:
+            start = best = val['loss']
+        if val['loss'] < best:
+            best = val['loss']
+    assert best < start  # should have made progress!
+
+
+def test_build_dict(exp):
+    regs = theanets.regularizers.from_kwargs(
+        exp, regularizers=dict(input_noise=0.01))
+    assert len(regs) == 1
+
+
+def test_build_list(exp):
+    reg = theanets.regularizers.Regularizer.build('weight_l2', 0.01)
+    regs = theanets.regularizers.from_kwargs(exp, regularizers=[reg])
+    assert len(regs) == 1
+
+
+@pytest.mark.parametrize('key, value', [
+    ('input_noise', 0.1),
+    ('input_dropout', 0.2),
+    ('hidden_noise', 0.1),
+    ('hidden_dropout', 0.2),
+    ('noise', {'*:out': 0.1}),
+    ('dropout', {'hid?:out': 0.2}),
+    ('hidden_l1', 0.1),
+    ('weight_l1', 0.1),
+    ('weight_l2', 0.01),
+    ('contractive', 0.01),
+])
+def test_sgd(key, value, exp):
+    assert_progress(exp, **{key: value})
+
+
+class RNN:
     @pytest.fixture
-    def exp(self):
-        return theanets.Regressor([self.NUM_INPUTS, 20, self.NUM_OUTPUTS], rng=131)
-
-    def assert_progress(self, **kwargs):
-        start = best = None
-        for _, val in self.exp().itertrain(
-                [self.INPUTS, self.OUTPUTS],
-                algorithm='sgd',
-                patience=2,
-                min_improvement=0.01,
-                max_gradient_norm=1,
-                batch_size=self.NUM_EXAMPLES,
-                **kwargs):
-            if start is None:
-                start = best = val['loss']
-            if val['loss'] < best:
-                best = val['loss']
-        assert best < start   # should have made progress!
-
-
-class TestBuild(Mixin, util.Base):
-    def test_regularizers_dict(self, exp):
-        regs = theanets.regularizers.from_kwargs(
-            exp, regularizers=dict(input_noise=0.01))
-        assert len(regs) == 1
-
-    def test_regularizers_list(self, exp):
-        reg = theanets.regularizers.Regularizer.build('weight_l2', 0.01)
-        regs = theanets.regularizers.from_kwargs(exp, regularizers=[reg])
-        assert len(regs) == 1
-
-
-class TestNetwork(Mixin, util.Base):
-    def test_input_noise(self):
-        self.assert_progress(input_noise=0.001)
-
-    def test_input_dropout(self):
-        self.assert_progress(input_dropout=0.1)
-
-    def test_hidden_noise(self):
-        self.assert_progress(hidden_noise=0.001)
-
-    def test_hidden_dropout(self):
-        self.assert_progress(hidden_dropout=0.1)
-
-    def test_noise(self):
-        self.assert_progress(noise={'*:out': 0.001})
-
-    def test_dropout(self):
-        self.assert_progress(dropout={'*:out': 0.1})
-
-    def test_hidden_l1(self):
-        self.assert_progress(hidden_l1=0.001)
-
-    def test_weight_l1(self):
-        self.assert_progress(weight_l1=0.001)
-
-    def test_weight_l2(self):
-        self.assert_progress(weight_l2=0.001)
-
-    def test_contractive(self):
-        self.assert_progress(contractive=0.001)
-
-
-class TestRecurrent(Mixin, util.RecurrentBase):
-    @pytest.fixture
-    def exp(self):
+    def net(self):
         return theanets.recurrent.Regressor([
-            self.NUM_INPUTS, (20, 'rnn'), self.NUM_OUTPUTS], rng=131)
+            u.NUM_INPUTS, (u.NUM_HID1, 'rnn'), u.NUM_OUTPUTS], rng=131)
 
-    def test_recurrent_matching(self, exp):
-        regs = theanets.regularizers.from_kwargs(exp)
-        outputs, _ = exp.build_graph(regs)
+    def test_recurrent_matching(self, net):
+        regs = theanets.regularizers.from_kwargs(net)
+        outputs, _ = net.build_graph(regs)
         matches = theanets.util.outputs_matching(outputs, '*:out')
         hiddens = [(n, e) for n, e in matches if e.ndim == 3]
         assert len(hiddens) == 3, [n for n, e in hiddens]
 
-    def test_recurrent_norm(self):
-        self.assert_progress(recurrent_norm=dict(pattern='*:out', weight=0.001))
+    @pytest.mark.parametrize('key, value', [
+        ('recurrent_norm', dict(pattern='*:out', weight=0.1)),
+        ('recurrent_state', dict(pattern='*:out', weight=0.1)),
+    ])
+    def test_progress(self, key, value, net):
+        assert_progress(net, **{key: value})
 
-    def test_recurrent_norm_error(self):
+    @pytest.mark.parametrize('key, value', [
+        ('recurrent_norm', dict(pattern='*:out', weight=0.1)),
+        ('recurrent_state', dict(pattern='*:out', weight=0.1)),
+    ])
+    def test_raises(self, key, value, net):
         with pytest.raises(theanets.util.ConfigurationError):
-            self.assert_progress(recurrent_norm=0.001)
-
-    def test_recurrent_state(self):
-        self.assert_progress(recurrent_state=dict(pattern='*:out', weight=0.001))
-
-    def test_recurrent_state(self):
-        with pytest.raises(theanets.util.ConfigurationError):
-            self.assert_progress(recurrent_state=0.001)
+            assert_progress(net, **{key: value})

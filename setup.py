eb4ed4f0b1086a1f5322078d91d2132582a487e7
Leif Johnson <leif@lmjohns3.com>
2016-05-16 22:10:25 -0700

    Add a temporary dependency on nose-parameterized.

--- setup.py
+++ setup.py
@@ -22,7 +22,10 @@ setuptools.setup(
               'classifier '
               'theano '
               ),
-    install_requires=['climate', 'downhill', 'theano'],
+    install_requires=['climate', 'downhill', 'theano',
+                      # TODO(leif): remove when theano is fixed.
+                      'nose-parameterized',
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
